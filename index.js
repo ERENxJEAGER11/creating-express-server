84d2dbe358f891c94cde1bfac67810d00b0f816c
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2023-04-07 23:16:25 +0530

    getting JSON data from api

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');  //import express
 const app = express();  // create an instance of express
 const port = 3000    //local port
 
+const data = require('./data.json');   //import data
 
 // define route and api
 app.get('/', (req,res) => {
-    res.send('hi Aman')
+    res.send(data)
 })
 
 app.get('/route2', (req,res) => {
