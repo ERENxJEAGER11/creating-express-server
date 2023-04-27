d8465c92e5aafb625e7bb335862e6b21220f2194
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2023-04-27 23:41:30 +0530

    merging back-end and front-end

---
+++ front-end/src/Components/userCard/UserCard.jsx
@@ -0,0 +1,45 @@
+import './UserCard.css';
+import React, { useEffect, useState } from 'react';
+
+function UserCard() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('localhost:3001/data');
+        const json = await response.json();
+        setData(json);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id} className="card">
+          <img src={item.image} alt={item.title} />
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default UserCard;
